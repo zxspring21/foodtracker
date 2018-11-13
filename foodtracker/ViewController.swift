51e3788d2756580f010af5dd430ee5a3c64b19de
Portran <peterlu4248@icloud.com>
2018-11-13 12:06:43 +0800

    Initial Commit

---
+++ foodtracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodtracker
+//
+//  Created by 盧柏瑋 on 2018/11/13.
+//  Copyright © 2018 盧柏瑋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
