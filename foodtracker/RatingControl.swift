012ed68c074f5ae5b06007b6c1b62ece21e4a24a
zxspring21 <zxgpkm@gmail.com>
2019-04-03 17:29:22 +0800

    1. Deleting the setDefaultString Button content and appearance 2. Adjusting some hint, value, and label setting in controlling the star button in rating class subclass

--- foodtracker/RatingControl.swift
+++ foodtracker/RatingControl.swift
@@ -26,7 +26,11 @@ import UIKit
     }
     private var ratingButtons = [UIButton]()
     
-    var rating = 0
+    var rating = 0{
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupButtons()
@@ -43,11 +47,30 @@ import UIKit
     
     //MARK: Button Action
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
     private func setupButtons() {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
         // Create the button
         // This initializer calls init(frame:) and passes in a zero-sized rectangle, and default Auto Layout
         // But when you add a view to a stack view, the stack view automatically sets the view’s translatesAutoresizingMaskIntoConstraints property to false
@@ -55,16 +78,28 @@ import UIKit
         
         //The underscore (_) represents a wildcard, which you can use when you don’t need to know which iteration of the loop is currently executing.
         
-        for _ in 0..<starCount{
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        for index in 0..<starCount{
             let button = UIButton()
-            button.backgroundColor = UIColor.red
-            
+            // Set the button images
+            // Buttons have five different states: normal, highlighted, focused, selected, and disabled.
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             // Add constraints
             // If this property set True(default), then it tells layout engine to create constraints that define the view’s size and position based on the view’s frame and autoresizingmask properties.
             
             button.translatesAutoresizingMaskIntoConstraints = false
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            // Set the accessibility label
+            button.accessibilityLabel = "Set \(index + 1) star rating"
             
             // Setup the button action
             // It's attaching the ratingButtonTapped(_:) action method to the button object, which will be triggered whenever the .TouchDown event occurs
@@ -73,6 +108,36 @@ import UIKit
             addArrangedSubview(button)
             // Add the new button to the rating button array
             ratingButtons.append(button)
+        }
+        updateButtonSelectionStates()
+    }
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+            
+            // Set the hint string for the currently selected star
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            } else {
+                hintString = nil
+            }
+            
+            // Calculate the value string
+            let valueString: String
+            switch (rating) {
+            case 0:
+                valueString = "No rating set."
+            case 1:
+                valueString = "1 star set."
+            default:
+                valueString = "\(rating) stars set."
+            }
+            
+            // Assign the hint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
         }
     }
 }
