73d125f438fa4ca550b6a1b85ee2b88ed070314a
zxspring21 <zxgpkm@gmail.com>
2019-04-07 21:52:32 +0800

    1.Create a structure to store string constants 2.Understand the difference between static properties and instance properties 3.Use the NSCoding protocol to read and write data

--- foodtracker/Meal.swift
+++ foodtracker/Meal.swift
@@ -7,9 +7,32 @@
 //
 // Importing UIKit also gives you access to Foundation(default).
 import UIKit
-class Meal {
-    
+import os.log
+// The NSObject class’s only initializer is init(), the Swift compiler adds the call for you automatically, so you don’t need to change your code;
+/*
+     The NSCoding protocol declares two methods that any class that adopts to it must implement so that instances of that class can be encoded and decoded:
+        1.encode(with aCoder: NSCoder)
+        2.init?(coder aDecoder: NSCoder)
+ */
+
+
+class Meal: NSObject, NSCoding {
     //MARK: Properties
+    
+    //MARK: Archiving Paths
+    //You mark these constants with the static keyword, which means they belong to the class instead of an instance of the class.
+    //Outside of the Meal class, you’ll access the path using the syntax Meal.ArchiveURL.path. There will only ever be one copy of these properties, no matter how many instances of the Meal class you create.
+    //The DocumentsDirectory constant uses the file manager’s urls(for:in:) method to look up the URL for your app’s documents directory. This is a directory where your app can save data for the user. This method returns an array of URLs, and the first parameter returns an optional containing the first URL in the array
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    //MARK: Type
+    struct PropertyKey {
+        // The static keyword indicates that these constants belong to the structure itself, not to instances of the structure
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     var name: String
     var photo: UIImage?
@@ -32,5 +55,28 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    //The required modifier means this initializer must be implemented on every subclass, if the subclass defines its own initializers.
+    //The convenience modifier means that this is a secondary initializer, and that it must call a designated initializer from the same class.
+    //The question mark (?) means that this is a failable initializer that might return nil.
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        // Because photo is an optional property of Meal, just use conditional cast.
+        // You downcast the value returned by decodeObject(forKey:) as a UIImage, and assign it to the photo constant. If the downcast fails, it assigns nil to the photo property. There is no need for a guard statement here, because the photo property is itself an optional.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        // Must call designated initializer.
+        // As a convenience initializer, this initializer is required to call one of its class’s designated initializers before completing. As the initializer’s arguments, you pass in the values of the constants you created while archiving the saved data.
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
--- foodtracker/MealTableViewController.swift
+++ foodtracker/MealTableViewController.swift
@@ -23,6 +23,14 @@ class MealTableViewController: UITableViewController {
         // Automatically adding the Editing button, and using the edit button item provided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem
 
+        // Load any saved meals, otherwise load sample data.
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        }else {
+            // Load the sample data.
+            loadSampleMeals()
+        }
+        
         // Load the sample data.
         loadSampleMeals()
     }
@@ -73,6 +81,7 @@ class MealTableViewController: UITableViewController {
         if editingStyle == .delete {
             // Delete the row from the data source
             meals.remove(at: indexPath.row)
+            saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -143,6 +152,9 @@ class MealTableViewController: UITableViewController {
                 meals.append(meal)
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
+            
+            // Save the meals.
+            saveMeals()
         }
         
         
@@ -166,5 +178,17 @@ class MealTableViewController: UITableViewController {
             fatalError("Unable to instantiate meal2")
         }
         meals += [meal1, meal2, meal3]
+    }
+    
+    private func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        if isSuccessfulSave {
+            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        } else {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
+    }
+    private func loadMeals() -> [Meal]? {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
     }
 }
